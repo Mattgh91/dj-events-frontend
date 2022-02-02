b48933134fe521eadeffd77b371141157d92b9de
Matt Haynes <Matthew.haynes@joipolloi.com>
2022-02-02 14:36:42 +0000

    Scaffold initial next.js project / some baisc pages + routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function AboutPage() {
+    return (
+        <div className={styles.container}>
+            <h1>Hello</h1>
+            <p>This is an app to find the latest DJ and other musical events</p>
+            <small>v 1.0.0</small>
+        </div>
+    )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/Home.module.css'
+
+export default function AddEventsPage() {
+    return (
+        <div className={styles.container}>
+            <p>Add an event</p>
+        </div>
+    )
+}
