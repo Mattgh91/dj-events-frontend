5a379f9c787c8b6962b96496b0cfd5318c227c30
Matt Haynes <Mattgh9152@gmail.com>
2022-02-02 19:42:21 +0000

    getStaticPaths added for events

--- pages/events/[slug].jsx
+++ pages/events/[slug].jsx
@@ -1,9 +1,43 @@
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function EventPage() {
+export default function EventPage({
+    theEvent: {
+        name,
+    }
+}) {
     return (
         <Layout>
-            <p>An event</p>
+            <h1>{name}</h1>
         </Layout>
     )
+}
+
+export async function getStaticPaths() {
+    const res = await fetch(`${API_URL}/api/events/`);
+    const events = await res.json();
+
+    const paths = events.map(evt => ({
+        params: {
+            slug: evt.slug,
+        }
+    }));
+
+    return {
+        paths,
+        fallback: false,
+    }
+}
+
+export async function getStaticProps({
+    params: { slug }
+}) {
+    console.log({slug});
+    const res = await fetch(`${API_URL}/api/events/${slug}`);
+    const theEvent = await res.json();
+
+    return {
+        props: { theEvent: theEvent[0] },
+        revalidate: 1,
+    }
 }
