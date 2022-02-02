9c7fd26d4369bf306307a95181772213dec30c88
Matt Haynes <Mattgh9152@gmail.com>
2022-02-02 17:57:19 +0000

    Display events from API on homepage/events pages

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,9 +1,29 @@
-import Layout from '@/components/Layout';
+import Link from 'next/link';
+import Image from 'next/image';
 
-export default function EventsPage() {
+import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
+import { API_URL } from '@/config/index';
+
+export default function EventsPage({ events }) {
     return (
         <Layout>
-            <p>Events</p>
+            <h1>Events</h1>
+            {events.length === 0 && <h2>No events to show</h2>}
+
+            {events.map(evt => (
+                <EventItem event={evt} key={evt.id} />
+            ))}
         </Layout>
     )
+}
+
+export async function getStaticProps() {
+    const res = await fetch(`${API_URL}/api/events`);
+    const events = await res.json();
+
+    return {
+        props: { events },
+        revalidate: 1, // tries to find if events have changed to save using getServerSideProps
+    };
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,24 @@
 import Link from 'next/link';
 
 import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
 import { API_URL } from '@/config/index';
 
 export default function Home({ events }) {
     return (
         <Layout>
             <h1>Upcoming Events</h1>
+            {events.length === 0 && <h2>No events to show</h2>}
+
+            {events.map(evt => (
+                <EventItem event={evt} key={evt.id} />
+            ))}
+
+            {events.length > 0 && (
+                <Link href="/events">
+                    <a className="btn-secondary">View all events</a>
+                </Link>
+            )}
         </Layout>
     )
 }
@@ -16,7 +28,7 @@ export async function getStaticProps() {
     const events = await res.json();
 
     return {
-        props: { events },
+        props: { events: events.slice(0, 3) },
         revalidate: 1, // tries to find if events have changed to save using getServerSideProps
     };
 }
