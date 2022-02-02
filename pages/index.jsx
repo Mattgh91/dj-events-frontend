f5c743b13aa919bfedc1da6456dd4a833b18958c
Matt Haynes <Mattgh9152@gmail.com>
2022-02-02 17:29:27 +0000

    Implement getStaticProps call to retreive event data from the API

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,22 @@
 import Link from 'next/link';
 
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function Home() {
+export default function Home({ events }) {
     return (
         <Layout>
-            <div><h1>Home</h1></div>
+            <h1>Upcoming Events</h1>
         </Layout>
     )
+}
+
+export async function getStaticProps() {
+    const res = await fetch(`${API_URL}/api/events`);
+    const events = await res.json();
+
+    return {
+        props: { events },
+        revalidate: 1, // tries to find if events have changed to save using getServerSideProps
+    };
 }
