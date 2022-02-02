15f6fc30e1bfb9ac78637e09d1bce8130ae6b867
Matt Haynes <Matthew.haynes@joipolloi.com>
2022-02-02 15:58:27 +0000

    Rename files from .js to .jsx; add custom 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import { FaExclamationTriangle } from 'react-icons/fa';
+import Layout from '../components/Layout';
+import styles from '../styles/404.module.css';
+
+const NotFoundPage = () => (
+    <Layout title="Page not found">
+        <div className={styles.error}>
+            <h1>
+                <FaExclamationTriangle /> 404
+            </h1>
+            <h2>Sorry, page not found</h2>
+            <Link href="/">Go back home</Link>
+        </div>
+    </Layout>
+);
+
+export default NotFoundPage;
