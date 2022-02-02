fd33f7632594f52cb95b29e64d7b5ce7fa6e6ff2
Matt Haynes <Mattgh9152@gmail.com>
2022-02-02 16:36:12 +0000

    Add showcase component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,7 @@
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Header from './Header';
+import Showcase from './Showcase';
 import Footer from './Footer';
 import styles from '@/styles/Layout.module.css'
 
@@ -9,6 +11,8 @@ export default function Layout({
     description,
     children,
 }) {
+    const router = useRouter();
+
     return (
         <div>
             <Head>
@@ -16,8 +20,9 @@ export default function Layout({
                 <meta name="description" content={description} />
                 <meta name="keywords" content={keywords} />
             </Head>
-
             <Header />
+
+            {router.pathname === '/' ? <Showcase /> : ''}
 
             <div className={styles.container}>
                 {children}
